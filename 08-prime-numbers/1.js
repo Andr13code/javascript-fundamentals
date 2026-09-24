655ce3887b869339b29693d5464109947a4dcab7
Andreas <andrprasetia@gmail.com>
2026-09-24 18:40:21 +0700

    Fix: perbaikan syntax di 08-prime-numbers

--- 08-prime-numbers/1.js
+++ 08-prime-numbers/1.js
@@ -1,9 +1,25 @@
 const readline = require('readline-sync');
 
-const enterNumber = +readline.question('Enter a number: ');
+const primeCount = +readline.question('Enter the number of prime numbers: ');
 
-if (enterNumber % 2 === 0) {
-    console.log(`${enterNumber} is even`);
-} else {
-    console.log(`${enterNumber} is odd`);
-};
+console.log(`The first ${primeCount} prime numbers are`);
+
+let count = 0;
+let number = 2;
+
+while (count < primeCount) {
+    if (isPrime(number)) {
+        console.log(number);
+        count++;
+    }
+    number++
+}
+
+function isPrime(number) {
+ for (let divisor = 2; divisor < number; divisor++) {
+    if (number % divisor === 0) {
+        return false;
+    }
+ }   
+    return true;
+}
